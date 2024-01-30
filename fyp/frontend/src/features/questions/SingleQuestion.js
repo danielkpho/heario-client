d28520342fec1ab4f3f7b3a477a3e8fa0c12109d
Daniel Kwok Pin Ho <dxh253@student.bham.ac.uk>
2024-01-30 16:14:49 +0000

    removed old fyp

--- fyp/frontend/src/features/questions/SingleQuestion.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { currentQuestion } from "./questionsSlice";
-import { useSelector } from "react-redux";
-
-export default function SingleQuestion() {
-  const question = useSelector(currentQuestion);
-  if (!question) return null;
-  return <div className="single-question">{question}</div>;
-}
