71b0537729cc1d653ca013571a0ccb256b40a95e
Daniel Kwok Pin Ho <dxh253@student.bham.ac.uk>
2024-01-30 16:14:49 +0000

    removed old fyp

---
+++ src/features/Game.js
@@ -0,0 +1,351 @@
+import React, { useEffect, useState, lazy, Suspense } from "react";
+import { lazyWithPreload} from "react-lazy-with-preload"
+import { socket } from "../api/socket";
+import { useDispatch, useSelector } from "react-redux";
+
+import { instrument as soundfontInstrument } from "soundfont-player";
+
+import { setIsStarted, resetState, setScores, incrementRound, incrementQuestion, resetGame, setIsRoundOver, setIsGameOver, selectTimer, setStatus } from "./gameSlice";
+import { newQuestion, resetStats, allQuestions, allTries, allAccuracy } from "./statSlice.js"
+import { setAnswers, setQuestionType, setTone, setCorrectAnswer, correctAns, currentTone } from "./questionsSlice";
+
+
+import Timer from "./Timer";
+import Leaderboard from "./Leaderboard";
+import Statistics from "./Statistics";
+import { TonesAnswerButton } from "./TonesAnswerButtons";
+import Leaderboard2 from "./Leaderboard2";
+
+import { Grid, Button, Backdrop } from "@mui/material";
+import { VolumeUp } from "@mui/icons-material";
+
+import Axios from "axios";
+
+
+export default function Game(){
+    const id = useSelector(state => state.game.id);
+    const dispatch = useDispatch();
+    const hostId = useSelector(state => state.game.hostId);
+    const isHost = socket.id === hostId;
+    const roundOver = useSelector(state => state.game.isRoundOver);
+    const [isPianoReady, setIsPianoReady] = useState(false);
+    const [piano, setPiano] = useState(null);
+    const roundSettings = useSelector(state => state.game.roundSettings);
+    const duration = useSelector(selectTimer);
+    const roundCount = useSelector(state => state.game.roundCount);
+    const correctAnswer = useSelector(correctAns);
+    const isGameOver = useSelector(state => state.game.isGameOver);
+    const questionType = useSelector(state => state.questions.questionType);
+    const tone = useSelector(currentTone);
+    const [showBackdrop, setShowBackdrop] = useState(false);
+    const username = localStorage.getItem("username");
+    const winner = useSelector(state => state.stats.winner);
+
+    const LazyReactPiano = lazyWithPreload(() => import("./Piano/Piano.js"));
+    const [pianoLoaded, setPianoLoaded] = useState(false);
+
+    useEffect(() => {
+        const loadReactPiano = async () => {
+            // Assuming ReactPiano has an asynchronous initialization method
+            await LazyReactPiano.preload();
+            setPianoLoaded(true);
+          };
+      
+          if (!pianoLoaded) {
+            loadReactPiano();
+          }
+        }, [pianoLoaded]);
+
+    let somePiano;
+    useEffect(() => { // fix loading 
+        const ac = new AudioContext();
+        soundfontInstrument(ac, 'acoustic_grand_piano', { soundfont: 'MusyngKite' })
+          .then((acoustic_grand_piano) => {
+            somePiano = acoustic_grand_piano;
+            setPiano(acoustic_grand_piano);
+            console.log("piano is ready");
+            setIsPianoReady(true);
+          });
+    
+        return () => {
+          ac.close();
+        };
+      }, []);
+
+    useEffect(() => {
+        if(isPianoReady){
+            if (socket.id === hostId){ // emit the question to the server if the user is the host
+                console.log("emitted get question");
+                socket.emit("getQuestion", id);
+            };
+            return () => {
+                socket.off("gameStarted");
+                socket.off("question");
+            };
+        }
+    }, [isPianoReady]);
+
+    useEffect(() => {
+            // socket.emit("getCorrectAnswer", { roomId: id });
+            socket.on("correctAnswer", (correctAnswer) => {
+                dispatch(setCorrectAnswer(correctAnswer));
+                dispatch(newQuestion(correctAnswer));
+            });
+            // socket.emit("getTone", { roomId: id });
+            socket.on("tone", (tone) => {
+                dispatch(setTone(tone));
+                console.log("received tone: "+ tone);
+                handlePlayNote(tone);
+            });
+            // socket.emit("getAnswers", { roomId: id });
+            socket.on("answers", (answers) => {
+                dispatch(setAnswers(answers));
+            });
+            socket.on("questionType", (questionType) => {
+                dispatch(setQuestionType(questionType));
+            });
+            return () => {
+                socket.off("correctAnswer");
+                socket.off("tone");
+                socket.off("answers");
+                
+            };
+    }, [dispatch]);
+
+    async function handlePlayNote() {
+        if (piano) {
+            try { 
+                if (Array.isArray(tone)) {
+                    if (questionType === 'scales'){
+                    // Play notes sequentially with a delay
+                        for (const note of tone) { 
+                            await piano.play(note);
+                            // Introduce a delay between notes
+                            await new Promise(resolve => setTimeout(resolve, 1000));
+                        }
+                        console.log("Played notes: " + tone.join(', '));
+                    } else {
+                        await Promise.all(tone.map(note => piano.play(note)));
+                        console.log("Played notes: " + tone.join(', '));
+                    }
+                } else {
+                    console.error("Invalid tone format:", tone);
+                }
+                
+            } catch (error) {
+                console.error("Error playing notes:", error);
+            }
+        } else {
+            console.log("Piano not ready");
+        }
+    }
+
+    function nextRound(){
+        socket.emit("nextRound", { roomId: id });
+    }
+        
+
+    useEffect(() => {
+        socket.on("nextRound", () => {
+            console.log("roundCount: " + roundCount);
+            if(roundCount < roundSettings.rounds){
+                if(socket.id === hostId){
+                    socket.emit("getQuestion", id);
+                    socket.emit("startTimer", { roomId: id, duration })
+                }
+                dispatch(incrementRound());
+                dispatch(incrementQuestion());
+                dispatch(setIsRoundOver(false));
+            } else {
+                dispatch(setIsGameOver(true));
+                dispatch(setStatus("Leaderboard"));
+            }
+        });
+        return () => {
+            socket.off("nextRound");
+        };
+    }, [roundCount, roundSettings.rounds, duration, dispatch]);
+
+    useEffect(( ) => {
+        socket.on("scores", ({ scores }) => {
+            dispatch(setScores(scores));
+        });
+        return () => {
+            socket.off("scores");
+        };
+    }, [dispatch]);
+
+    function handleOpen(){
+        setShowBackdrop(true);
+    }
+    function handleClose(){
+        setShowBackdrop(false);
+    }
+
+    function handleLeave(){
+        if (username === winner){ 
+            Axios.post("http://localhost:8000/incrementGamesWon", {
+                username,
+            } , {
+            }).then((response) => {
+                console.log(response);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+        socket.emit("leaveRoom", { roomId: id });
+        dispatch(resetState());
+        dispatch(resetStats())
+    }
+
+    function restartGame(){
+        if (username === winner){ 
+            Axios.post("http://localhost:8000/incrementGamesWon", {
+                username,
+            } , {
+            }).then((response) => {
+                console.log(response);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+        socket.emit("resetGame", { roomId: id });
+        dispatch(resetGame());
+        dispatch(resetStats());
+    };
+
+    function isPiano(){
+        if (roundSettings.piano === 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    
+
+    return (
+        <div>
+            {!isGameOver && (
+                <div>
+                {!isPianoReady && (
+                    <p>loading piano...</p>
+                )}
+                {isPianoReady && (
+                <div>
+                    <div>
+                    <button onClick={() => restartGame()}>End Game</button>
+                    <button onClick={() => nextRound()}>Next Round</button>
+                    </div>
+                    <Grid container justifyContent="center" alignItems="center" spacing={2}>
+                        <Grid item>
+                            <Button 
+                                variant="contained"
+                                color="secondary"
+                                startIcon={<VolumeUp />}
+                                onClick={() => handlePlayNote()}
+                                style={{ width: 200 }}
+                            > 
+                                Play Note
+                                </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid container justifyContent="center" alignItems="center" spacing={2} padding={2}>
+                        <Grid item>
+                            {!roundOver && (
+                            <div>
+                                <div>
+                                {isPiano() && (
+                                    <Suspense fallback={<div>Loading...</div>}>
+                                    {/* Use LazyReactPiano instead of ReactPiano */}
+                                    <LazyReactPiano />
+                                    </Suspense>
+                                )}
+                                </div>
+                                <div>
+                                    <TonesAnswerButton />
+                                </div>
+                            </div>           
+                            )}
+                        </Grid>
+                    </Grid>
+                    <div>
+                        {roundOver && (
+                            <div>
+                                <div>
+                                    <Leaderboard />
+                                    <Leaderboard2 />
+                                </div>
+                                <div>
+                                    <p>The correct answer was {correctAnswer}</p>
+                                </div>
+                            </div>
+                        )}
+                        
+                        <div>
+                            <Timer />
+                        </div>
+                    </div>
+                </div>
+                )}
+            </div>
+            )}
+            {isGameOver && (
+                <Grid 
+                    container
+                    direction={"column"}
+                >
+                    <Grid item>
+                        <Leaderboard />
+                    </Grid>
+                    <Grid 
+                    container 
+                    direction={"row"} 
+                    justifyContent={"center"} 
+                    alignItems={"center"} 
+                    spacing={2}
+                    padding={3}
+                    >
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="error"
+                                onClick={() => handleLeave()}
+                            >
+                                Leave Lobby
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="info"
+                                onClick={() => handleOpen()}
+                            >
+                                Statistics
+                            </Button>
+                            <Backdrop
+                                sx= {{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                                open={showBackdrop}
+                                onClick={handleClose}
+                            >
+                                <Statistics />
+                            </Backdrop>
+                        </Grid>
+                    {isHost && (
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="info"
+                                onClick={() => restartGame()}
+                            >
+                                Reset Game
+                            </Button>
+                        </Grid>
+                    )}
+                    
+                    </Grid>
+                </Grid>
+            )}
+        </div>
+            );
+}
---
+++ src/features/Profile.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from "react";
+
+import Axios from "axios";
+
+import { Button, Grid, TableContainer, TableSortLabel, Table, TableHead, TableCell, TableRow, Paper } from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+
+export default function Profile(){
+    const username = localStorage.getItem("username");
+    const navigate = useNavigate();
+
+    const [gamesPlayed, setGamesPlayed] = useState(0);
+    const [gamesWon, setGamesWon] = useState(0);
+
+    const [data, setData] = useState([]);
+    
+    const [sortConfig, setSortConfig] = useState({key: null, direction: "ascending"})
+    
+    const requestSort = (key) => {
+        let direction = 'ascending';
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+          direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+        // Sort the data array based on the selected key and direction
+        const sortedData = [...data].sort((a, b) => {
+          if (direction === 'ascending') {
+            return a[key] > b[key] ? 1 : -1;
+          } else {
+            return a[key] < b[key] ? 1 : -1;
+          }
+        });
+        setData(sortedData);
+      };
+
+    useEffect(() => {
+        Axios.post("http://localhost:8000/getGamesPlayed", {
+            username: username,
+        }).then((response) => {
+            setGamesPlayed(response.data.gamesPlayed);
+            setGamesWon(response.data.gamesWon)
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
+    useEffect(() => {
+        Axios.post("http://localhost:8000/getAttempts", {
+            username: username
+        }).then((response) => {
+            console.log(response.data.result);
+            setData(response.data.result);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
+    
+    const logout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+        navigate("/");
+    };
+
+    return (
+        <Grid 
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            padding={5}
+        >
+            <Grid item
+                container
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+            >
+                <Grid item>
+                    <h2>Username: {username}</h2>
+                </Grid>
+                <Grid item>
+                    <h2>Games played: {gamesPlayed}</h2>
+                </Grid>
+                <Grid item>
+                    <h2>Games Won: {gamesWon} </h2>
+                </Grid>
+            </Grid>
+            <Grid 
+                container
+                direction="column"
+                justifyContent={"center"}
+                alignItems={"center"}
+                padding={2}
+                spacing={2}
+            >
+                <Grid item>
+                    <TableContainer sx= {{ maxWidth: 500 }} component={Paper}>
+                        <Table sx= {{ minWidth: 500 }}>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center" onClick={() => requestSort('question_type')}>
+                                    <TableSortLabel active={sortConfig.key === 'question_type'} direction={sortConfig.direction}>
+                                        Question Type
+                                    </TableSortLabel>
+                                    </TableCell>
+                                    <TableCell align="center" onClick={() => requestSort('question')}>
+                                    <TableSortLabel active={sortConfig.key === 'question'} direction={sortConfig.direction}>
+                                        Question
+                                    </TableSortLabel>
+                                    </TableCell>
+                                    <TableCell align="center" onClick={() => requestSort('Correct Attempts')}>
+                                    <TableSortLabel active={sortConfig.key === 'Correct Attempts'} direction={sortConfig.direction}>
+                                        Correct Attempts
+                                    </TableSortLabel>
+                                    </TableCell>
+                                    <TableCell align="center" onClick={() => requestSort('Total Attempts')}>
+                                    <TableSortLabel active={sortConfig.key === 'Total Attempts'} direction={sortConfig.direction}>
+                                        Total Attempts
+                                    </TableSortLabel>
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            {data.map((row, index) => (
+                                <TableRow key={index}>
+                                <TableCell align="center">{row.question_type}</TableCell>
+                                <TableCell align="center">{row.question}</TableCell>
+                                <TableCell align="center">{row.correct_attempts}</TableCell>
+                                <TableCell align="center">{row.total_attempts}</TableCell>
+                                </TableRow>
+                            ))}
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item
+                container
+                direction="row"
+                padding={2}
+                spacing={2}
+                justifyContent={"center"}
+                alignItems={"center"}
+                >
+                    <Grid item>
+                        <Button variant="contained" onClick={() => navigate("/")}>Back</Button>
+                    </Grid>
+                    <Grid item>
+                        <Button color="error" variant="contained" onClick={logout}>Logout</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
