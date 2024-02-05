2a79194d5571244c09c6bafeb0c84f33e89cea1d
Daniel Kwok Pin Ho <dxh253@student.bham.ac.uk>
2024-02-05 19:00:56 +0000

    removed / from socket

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -3,7 +3,7 @@ console.log("Attempting to connect to server");
 // export const socket = io('http://localhost:8000', {
 //     withCredentials: true,
 // });
-export const socket = io('https://heario-13b5b094cc85.herokuapp.com/', {
+export const socket = io('https://heario-13b5b094cc85.herokuapp.com', {
     withCredentials: true,
 });
 
